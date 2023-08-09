f6681e7d1cb6c36bf9b125cf3167cd5bb8990fd0
khanh2kjr <khanhthachxa@gmail.com>
2023-08-10 00:11:18 +0700

    feat: change url api

--- client/src/constants/api.constant.js
+++ client/src/constants/api.constant.js
@@ -3,4 +3,4 @@ export const HEADER_DEFAULT = {
   'Content-Type': 'application/json',
 }
 export const TIMEOUT = 300000
-export const BASE_URL = 'https://vocabulary-service-wuci.onrender.com'
+export const BASE_URL = 'https://vocabulary-service-wuci.onrender.com/api'
